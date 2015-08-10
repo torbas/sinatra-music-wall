e3c58fb5a941d882f97f188e38d539b2121cfa37
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-10 21:57:25 +0000

    improve regex

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   validates :author, :title, presence: true
 
   def create_url
-    url =~ /\Ahttp:\/\// ? url : "http://" << url
+    url =~ /\Ahttps{0,1}:\/{2}/ ? url : "http://" << url
   end
 
 end

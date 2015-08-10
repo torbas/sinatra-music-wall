dc4360fa7b27bfa864af6fa58368fc2fe74d6fd6
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-10 22:23:25 +0000

    create users

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810183105) do
+ActiveRecord::Schema.define(version: 20150810202741) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "author"
     t.string   "title"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

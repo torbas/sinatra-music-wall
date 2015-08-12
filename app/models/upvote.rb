c6ddf5c4494db31f01915c293b0858301a211081
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-12 02:40:55 +0000

    upvote icon change

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,6 +1,7 @@
 class Upvote < ActiveRecord::Base
   belongs_to :user
   belongs_to :song
+  validates :user_id, :song_id, presence: true
   validates :user_id, uniqueness: { scope: :song_id,
     message: "Can only vote on this once" }
   after_save :update_song_upvote
